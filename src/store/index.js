2664bb23d603e72669c3247723ddf0394b771d33
孙培峰 <838344306@qq.com>
2019-09-20 20:53:27 +0800

    Token处理-将数据持久化到本地存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     // 1. 初始化一个数据
-    user: null
+    user: getItem('user')
   },
   mutations: {
     // 2. 添加一个 mutation 用于修改 state
     // 3. 登录成功，调用 mutation 将 token 放到 Vuex 容器中
     setUser (state, user) {
       state.user = user
+      // 4. 为了防止页面刷新数据丢失，所以还需要将数据存储到本地存储
+      setItem('user', state.user)
     }
   },
   actions: {
