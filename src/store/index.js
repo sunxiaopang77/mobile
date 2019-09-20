22bab304cab9a66ed67fafe666d11eb8ae1d0abd
孙培峰 <838344306@qq.com>
2019-09-20 20:46:41 +0800

    Token处理-将token存储到Vuex容器

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    // 1. 初始化一个数据
+    user: null
   },
   mutations: {
-
+    // 2. 添加一个 mutation 用于修改 state
+    // 3. 登录成功，调用 mutation 将 token 放到 Vuex 容器中
+    setUser (state, user) {
+      state.user = user
+    }
   },
   actions: {
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -85,9 +85,8 @@ export default {
 
         // 验证通过，提交表单
         this.isLoginLoading = true
-        const { data } = await login(this.user)
-
-        console.log(data)
+        const res = await login(this.user)
+        this.$store.commit('setUser', res.data.data)
         this.$toast.success('登录成功')
       } catch (err) {
         if (err.response && err.response.status === 400) {
